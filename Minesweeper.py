d764ae79aa55eecf384ab7a25b794e27c7d44bed
habiibn <habibnurkholis25@gmail.com>
2023-03-06 06:35:17 +0700

    create main function

--- Minesweeper.py
+++ Minesweeper.py
@@ -3,3 +3,19 @@ import math
 import random
 import re
 import sys
+
+# The function is expected to return a STRING_ARRAY
+# The function accepts following parameters
+# 1. INTEGER N
+# 2. STRING_ARRAY bom
+
+def minesweeper(N, bom):
+    # 1. Cek lokasi dari bom
+    # 2. Cek apakah bom berada di sisi samping
+    # 3. Assign apakah angka 1 di 1 grid jarak dari bom
+    # 4. Apabila terdapat angka 1 di sekitar beberapa bom, maka dijumlahkan
+    
+    if (N > 50):
+        print("Jumlah bom melebihi 50")
+        pass
+
